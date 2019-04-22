bcd237c1dfe577662ae54b3dd4abcb2878412f82
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-22 13:45:03 -0400

    Move renderMessage function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Messages from "./Messages";
 
@@ -14,6 +13,7 @@ function randomName() {
 function randomColor() {
   return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
+
 
 class App extends Component {
 
@@ -32,47 +32,15 @@ class App extends Component {
       color: randomColor()
     }
   }
-  
-  renderMessage(message) {
-    const {member, text} = message;
-    const {currentMember} = this.props;
-    const messageFromMe = member.id === currentMember.id;
-    const className = messageFromMe ?
-      "Messages-message currentMember" : "Messages-message";
-    return (
-      <li className={className}>
-        <span
-          className="avatar"
-          style={{backgroundColor: member.clientData.color}}
-        />
-        <div className="Message-content">
-          <div className="username">
-            {member.clientData.username}
-          </div>
-          <div className="text">{text}</div>
-        </div>
-      </li>
-    );
-  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Messages
+        messages={this.state.messages}
+        currentMember={this.state.member}
+      />
+    </div>
     );
   }
 }
