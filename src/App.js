7c8ee58d95dcfb4c5dd8a87fca93f18224c126c0
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-22 11:30:14 -0400

    Update App.js
    Render message as list item with sender's name and avatar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+  renderMessage(message) {
+    const {member, text} = message;
+    const {currentMember} = this.props;
+    const messageFromMe = member.id === currentMember.id;
+    const className = messageFromMe ?
+      "Messages-message currentMember" : "Messages-message";
+    return (
+      <li className={className}>
+        <span
+          className="avatar"
+          style={{backgroundColor: member.clientData.color}}
+        />
+        <div className="Message-content">
+          <div className="username">
+            {member.clientData.username}
+          </div>
+          <div className="text">{text}</div>
+        </div>
+      </li>
+    );
+  }
+  
   render() {
     return (
       <div className="App">
